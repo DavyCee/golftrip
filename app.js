105b233b029989f344a166e3b988c0b82c254853
DavyCee <dcanavan96@gmail.com>
2026-08-30 10:53:47 +0100

    Refactor biggest win display logic in app.js

--- app.js
+++ app.js
@@ -2884,12 +2884,16 @@ function renderStats() {
             ? `
                 <div class="biggest-win">
 
-                    <div class="biggest-win-player">
-                        ${biggestRyderWin.winner}
-                    </div>
+                    <div class="biggest-win-side">
 
-                    <div class="biggest-win-team">
-                        (${biggestRyderWin.winnerTeam})
+                        <span class="biggest-win-player">
+                            ${biggestRyderWin.winner}
+                        </span>
+
+                        <span class="biggest-win-team">
+                            (${biggestRyderWin.winnerTeam})
+                        </span>
+
                     </div>
 
 
@@ -2898,33 +2902,26 @@ function renderStats() {
                     </div>
 
 
-                    <div class="biggest-win">
+                    <div class="biggest-win-side">
 
-    <div class="biggest-win-side">
-        <span class="biggest-win-player">
-            ${biggestRyderWin.winner}
-        </span>
+                        <span class="biggest-win-player">
+                            ${biggestRyderWin.loser}
+                        </span>
 
-        <span class="biggest-win-team">
-            (${biggestRyderWin.winnerTeam})
-        </span>
-    </div>
+                        <span class="biggest-win-team">
+                            (${biggestRyderWin.loserTeam})
+                        </span>
 
+                    </div>
 
-    <div class="biggest-win-result">
-        Won by ${biggestRyderWin.result} vs
-    </div>
-
-
-    <div class="biggest-win-side">
-        <span class="biggest-win-player">
-            ${biggestRyderWin.loser}
-        </span>
-
-        <span class="biggest-win-team">
-            (${biggestRyderWin.loserTeam})
-        </span>
-    </div>
+                </div>
+            `
+            : `
+                <div class="biggest-win-player">
+                    No completed matches
+                </div>
+            `
+    }
 
 </div>
 
